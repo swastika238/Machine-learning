a729e8c6344030869acaf14bee78fed4da3cba04
Swastika Dhakal <swastikadhakal37@gmail.com>
2026-09-03 14:47:05 +0545

    refractor: update  setup.py

--- ml-project/setup.py
+++ ml-project/setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup,find_packages
 
+
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path, 'r') as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
 setup(
     name='ml-project',
     version='0.0.1',
     author='swastika',
     author_email='swastikadhakal376@gmail.com' ,
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'flask',
-        'scikit-learn',
-        'seaborn',
-        'matplotlib',
-        'xgboost',
-        'streamlit',
-    ],
+    install_requires=get_requirements('requirements.txt')   
+       
 )
